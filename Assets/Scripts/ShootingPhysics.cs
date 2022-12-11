3a3eff0bf96089079c93694579ebeda3f61a0375
Pau Marin <paumarin96@gmail.com>
2022-12-11 18:03:30 +0100

    started exercise 2

--- Assets/Scripts/ShootingPhysics.cs
+++ Assets/Scripts/ShootingPhysics.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 
@@ -19,6 +20,15 @@ public class ShootingPhysics : MonoBehaviour
     private Vector3 finalPos;
     private Vector3 initialPos;
     private Vector3 initialVel;
+
+    private Vector3 eulerOldPos;
+    private Vector3 eulerOldVel;
+    private Vector3 eulerAccel;
+    private Vector3 eulerForces;
+
+    private Vector3 w;
+    [SerializeField] private float mass = 1;
+
 
     public void OnEnable()
     {
@@ -54,6 +64,19 @@ public class ShootingPhysics : MonoBehaviour
         initialVel.x = (target.position.x - initialPos.x) / totalAnimationTime;
         initialVel.y = ((target.position.y - initialPos.y) - (0.5f * acceleration * totalAnimationTime * totalAnimationTime)) / totalAnimationTime ;
         initialVel.z = (target.position.z - initialPos.z) / totalAnimationTime;
+        
+
+        
+        var torque = Vector3.Cross(ball.position + ball.right * -0.5f, 1 * (target.position - initialPos).normalized);
+        w = Time.deltaTime * (torque / (2.0f / 5.0f * mass * (0.5f * 0.5f))); 
+        Debug.Log(w);
+       
+        
+
+        initialVel = target.position - initialPos;
+        eulerOldVel = Vector3.forward;//initialVel * 2;
+        
+        Debug.Log(w);
 
     }
 
@@ -66,15 +89,25 @@ public class ShootingPhysics : MonoBehaviour
 
         if (timer >= totalAnimationTime)
         {
-            ResetTimer();
+            //ResetTimer();
         }
             
 
-        finalPos.x = initialPos.x + initialVel.x * timer;
-        finalPos.y = initialPos.y + initialVel.y * timer + (acceleration * timer * timer)/2;
-        finalPos.z = initialPos.z + initialVel.z * timer;
-        
+        // finalPos.x = initialPos.x + initialVel.x * timer;
+        // finalPos.y = initialPos.y + initialVel.y * timer + (acceleration * timer * timer)/2;
+        // finalPos.z = initialPos.z + initialVel.z * timer;
 
+        Vector3 magnusForce = 2.0f * (Vector3.Cross(w, eulerOldVel));
+        //Debug.Log(magnusForce);
+        //Vector3 targetForce = (target.position - initialPos) * 10;
+        eulerForces = magnusForce;// + Vector3.down * (2 * 9.81f);
+        eulerAccel = eulerForces / 1.0f;
+
+        Vector3 eulerVel = eulerOldVel + eulerAccel * Time.deltaTime;
+        finalPos = ball.position + eulerVel * Time.deltaTime;
+
+        eulerOldVel = eulerVel;
+        
 
         ball.position = finalPos; 
 
